59d719318e5d10b881deeea784686c4ab1e56f44
tharvey <tharveyster@gmail.com>
2021-09-24 10:40:13 -0400

    Added GET routes for /api/workouts and /api/workouts/range

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
@@ -26,7 +26,37 @@ app.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
-// pending...
+app.get("/api/workouts", (req, res) => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    }
+  ])
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
+app.get("/api/workouts/range", function (req, res) {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        dateDifference: { $subtract: [new Date(), "$day"], },
+      }
+    },
+  ])
+  .then(function (dbWorkouts) {
+    res.json(dbWorkouts);
+  });
+});
+
+// TODO - Add /api/workouts/ POST route
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
